e8de3f1210c55de52d687e192a7c25d3205a5d90
colinh09 <colin.hwang@cooper.edu>
2022-09-12 00:56:05 -0400

    finished login page, added functionality with logging out and logging out

--- src/Header.js
+++ src/Header.js
@@ -8,15 +8,23 @@ import SupervisorAccountIcon  from '@mui/icons-material/SupervisorAccount';
 import BuisnessCenterIcon  from '@mui/icons-material/BusinessCenter';
 import ChatIcon  from '@mui/icons-material/Chat';
 import NotificationsIcon  from '@mui/icons-material/Notifications';
+import { useDispatch } from 'react-redux';
+import { auth } from './firebase';
+import { login, logout} from './features/userSlice';
 
-function header() {
+function Header() {
+  const dispatch = useDispatch();
+  const logoutOfApp = () => {
+    dispatch(logout())
+    auth.signOut();
+  }
   return (
     <div className = "header">
         <div className = "header__left">
             <LinkedInIcon color="primary" sx={{ fontSize: 40 }}/>
             <div className="header__search">
                 <SearchIcon />
-                <input type="text"></input>
+                <input placeholder="Search" type="text"></input>
             </div>
         </div>
 
@@ -26,10 +34,12 @@ function header() {
             <HeaderOption Icon={BuisnessCenterIcon} title='Jobs'/>
             <HeaderOption Icon={ChatIcon} title='Messaging'/>
             <HeaderOption Icon={NotificationsIcon} title='Notifications'/>
-            <HeaderOption avatar="https://images.unsplash.com/photo-1508185159346-bb1c5e93ebb4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=55cf14db6ed80a0410e229368963e9d8&auto=format&fit=crop&w=1900&q=80" title="me" />
+            <HeaderOption avatar="https://images.unsplash.com/photo-1508185159346-bb1c5e93ebb4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=55cf14db6ed80a0410e229368963e9d8&auto=format&fit=crop&w=1900&q=80" title="me" 
+              onClick={logoutOfApp}
+            />
         </div>
 
     </div>
   )
 }
-export default header
+export default Header
